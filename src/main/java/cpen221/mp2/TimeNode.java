91483d3bed5fd67a4fe71c19fe6d597de8a8a7f5
halab1t <laura.romer@gmail.com>
2021-10-29 21:11:57 -0700

    done with Interaction

--- src/main/java/cpen221/mp2/TimeNode.java
+++ src/main/java/cpen221/mp2/TimeNode.java
@@ -1,14 +1,20 @@
 package cpen221.mp2;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Comparator;
 
 public class TimeNode implements Comparable<TimeNode> {
     private final int time;
     private  int numEmails;
-    public TimeNode (int time1){
+    //TODO : Review safety of adding/ how we will work with this;
+    public TimeNode (int time1, int emails){
         time = time1;
-        numEmails = 1;
+        numEmails = emails;
+    }
+    public TimeNode(int time1){
+        time = time1;
+        numEmails = 0; // does this break the rep invariant?
     }
     public void add(){
         numEmails ++;
@@ -20,8 +26,19 @@ public class TimeNode implements Comparable<TimeNode> {
         return numEmails;
     }
     @Override
-    public int compareTo( TimeNode otherNode){
+    public int compareTo(TimeNode otherNode){
         return Integer.compare(getTime(),otherNode.getTime());
     }
-
+    @Override
+    public boolean equals(Object other){
+        if(!(other instanceof TimeNode)){
+            return false;
+        }
+        TimeNode node = (TimeNode) other;
+        return (this.getTime() == node.getTime());
+    }
+   /* @Override
+    public int hashCode(TimeNode node){
+        return node.getTime();
+    }*/
 }
