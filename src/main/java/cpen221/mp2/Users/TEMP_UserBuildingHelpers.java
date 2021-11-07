86dc93b59ba758655af17b5c2083befbf3036ad4
halab1t <laura.romer@gmail.com>
2021-11-07 11:19:12 -0800

    user helper stuff going to add comparators

--- src/main/java/cpen221/mp2/Users/TEMP_UserBuildingHelpers.java
+++ src/main/java/cpen221/mp2/Users/TEMP_UserBuildingHelpers.java
@@ -5,22 +5,24 @@ import cpen221.mp2.Interaction;
 import java.util.*;
 
 public class TEMP_UserBuildingHelpers {
-
+//TODO: remo
     private static Hashtable<Integer, Hashtable<Integer, Interaction>> graph;
     private static HashMap<Integer, UndirectedUser> users;
     private static ArrayList<Integer> usersSortedByActivity;
-
-    public static void createUserMap (){
-        Map<Integer, UndirectedUser> userMap = new HashMap<>();
+//TODO: getsent/ getrecieved replace with get interactions that takes in enum
+    public static HashMap<Integer, UndirectedUser>
+    createUserMapUDW (Hashtable<Integer, Hashtable<Integer, Interaction>> graph){
+        HashMap<Integer, UndirectedUser> userMap = new HashMap<>();
         for (Integer user1 : graph.keySet()) {
             userMap.put(user1, new UndirectedUser(user1));
             for (Integer user2: graph.get(user1).keySet()) {
                 userMap.get(user1).interactWithUser(user2, graph.get(user1).get(user2).getEmailCount());
             }
         }
+        return  userMap;
     }
 
-    public static void createUsersSortedByActivity() {
+    public static void createUsersSortedByActivity(HashMap) {
         // TODO: double check logic here - im tired
         List<UndirectedUser> userList = new ArrayList<>();
         for (Integer user : users.keySet()) {
@@ -56,13 +58,7 @@ class sortByActivity implements Comparator<User> {
             return 1;
         } else {
             // TODO: it wants me to use Integer.compare()...
-            if (user1.getUserID() < user2.getUserID()){
-                return 1;
-            } else if (user2.getUserID() < user1.getUserID()){
-                return -1;
-            } else {
-                return 0;
-            }
+            return Integer.compare(user2.getUserID(), user1.getUserID());
         }
     }
 }
